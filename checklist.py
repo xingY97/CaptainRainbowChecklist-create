f097bf60ffd48d2f8dae6763e111064364210c41
Xing_Ye <YOUR_PERSONAL_EMAIL@example.com>
2019-08-28 21:13:53 -0700

    create

---
+++ checklist.py
@@ -0,0 +1,13 @@
+
+
+checklist = list()
+checklist.append('Blue')
+print(checklist)
+checklist.append('Orange')
+print(checklist)
+
+def create(item):
+    checklist.append(item)
+
+create ('black')
+print(checklist)
