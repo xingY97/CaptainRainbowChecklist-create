f0255f5ec7f904e2c75d2550d6deed794ddc905e
Xing_Ye <YOUR_PERSONAL_EMAIL@example.com>
2019-09-02 14:00:05 -0700

    improved code

--- checklist.py
+++ checklist.py
@@ -1,5 +1,7 @@
-checklist = list ()
+import os
 
+os.system('clear')
+checklist = list ()
 
 #Create
 def create (item):
@@ -23,24 +25,40 @@ def list_all_items():
     index = 0
     for list_item in checklist:
         print("{} {}".format(index, list_item))
-        #print(str(index) + list_item)
         index += 1
+
 def mark_completed(index):
     update(index, "{}{}".format("√", checklist[int(index)]))
+
+def mark_uncompleted(index):
+    update(int(index), read(index)[1:])
 
 def select(function_code):
     #Create item
     if function_code == "C":
-        input_item = user_input("Input item:")
+        input_item = user_input("create item:")
         create(input_item)
     #Read item
     elif function_code == "R":
-        item_index = user_input("input Number?")
-
+        item_index = user_input("Read how many item?")
         read(item_index)
     #print all items
     elif function_code == "P":
         list_all_items()
+    
+    elif function_code == "U":
+        item_index = input ("which item do you want to be updated: ")
+        item_update = input("which item do you want instead")
+        update(item_index, item_update)
+
+    elif function_code == "RM":
+        item_index = input ("which item do you want to remove from list")
+        destory(item_index)
+    
+    elif function_code == "UC":
+        mark_uncompleted()
+        
+    #elif function_code == ""
     elif function_code == "Q":
         #THis is where we want to stop out loop
         return False  
@@ -59,36 +77,33 @@ def test():
     create("red cloak")
 
     print(read(0))
-    print(read(1))
     
     update(0,"purple socks")
     destory(1)
 
     print(read(0))
+    list_all_items()
 
     mark_completed(0)
-   
-    list_all_items()
 
-    #Your testing code here
-    #...
-    #call your new function with the appropriate value
+    mark_uncompleted(0)
+
     select("C")
-    #View the results
     list_all_items()
-    #Call function with new value
+    
     select("R")
-    #View results
     list_all_items()
-    #Continue until all code is run
 
-    user_value = user_input("Please Enter a value:")
-    print(user_value)
+
+    #user_value = user_input("Please Enter a value:")
+    #print(user_value)
 
 
 test()
+
 running = True
 while running: 
+    
     selection = user_input(
-    "Press C to add to list, R to Read from list and P to display list")
+    " Press C to create item to list\n R to Read item\n P to display all item\n U to update an item from list\n Press Q to Exit")
     running = select(selection)
